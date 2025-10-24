6d87286f50b30470b760f3e9666b0ba7599d4606
ArthurDanMac <macias.galvan.arturodaniel@gmail.com>
2025-10-24 10:37:54 -0600

    Primer commit 2

---
+++ app/src/main/java/edu/pdm/proyectounomacsosa/MainActivity.kt
@@ -0,0 +1,47 @@
+package edu.pdm.proyectounomacsosa
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.pdm.proyectounomacsosa.ui.theme.ProyectoUnoMacSosaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ProyectoUnoMacSosaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ProyectoUnoMacSosaTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/edu/pdm/proyectounomacsosa/ui/RegisterScreen.kt
@@ -0,0 +1,2 @@
+package edu.pdm.proyectounomacsosa.ui
+
