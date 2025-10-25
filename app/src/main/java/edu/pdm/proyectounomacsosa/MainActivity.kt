7fc9867acfa76a9f1987eaad145573349c031aea
ArthurDanMac <macias.galvan.arturodaniel@gmail.com>
2025-10-24 19:32:47 -0600

    List Screen mejorada

--- app/src/main/java/edu/pdm/proyectounomacsosa/MainActivity.kt
+++ app/src/main/java/edu/pdm/proyectounomacsosa/MainActivity.kt
@@ -3,32 +3,23 @@ package edu.pdm.proyectounomacsosa
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.room.Room
 import edu.pdm.proyectounomacsosa.model.AppDatabase
 import edu.pdm.proyectounomacsosa.model.TaskRepository
-import edu.pdm.proyectounomacsosa.ui.RegisterScreen
-import edu.pdm.proyectounomacsosa.ui.theme.ProyectoUnoMacSosaTheme
+import edu.pdm.proyectounomacsosa.ui.ListScreen
 import edu.pdm.proyectounomacsosa.viewmodel.TaskViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "users.db").build()
+        val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "tasks.db").build()
         val repo = TaskRepository(db.task_dao())
         val viewModel = TaskViewModel(repo)
         setContent {
             MaterialTheme {
-                RegisterScreen(
+                ListScreen(
                     viewModel,
                     onSearch = { Unit  },
                     navController = NavHostController(applicationContext)
---
+++ app/src/main/java/edu/pdm/proyectounomacsosa/ui/ListScreen.kt
@@ -0,0 +1,88 @@
+package edu.pdm.proyectounomacsosa.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import edu.pdm.proyectounomacsosa.model.Task
+import edu.pdm.proyectounomacsosa.viewmodel.TaskViewModel
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ListScreen(viewModel: TaskViewModel, onSearch: () -> Unit, navController: NavHostController) {
+    val tareas by viewModel.tasks.collectAsState()
+
+    LaunchedEffect(Unit) { viewModel.loadTasks() }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Listas de Tarea por Hacer") }
+            )
+        }
+    ) { padding ->
+        LazyColumn(Modifier.padding(padding)) {
+            items(tareas) { tarea ->
+                TaskItem (
+                    task = tarea,
+                    onCheckedChange = { isChecked -> /*es una funcion que verifica
+                                                                si cambio ese componente,
+                                                                para poder reconstruir*/
+                        tareas.map {
+                            if (it.id == tarea.id)
+                                it.copy(status = isChecked)
+                            else
+                                it
+                        }
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskItem(task: Task, onCheckedChange: (Boolean) -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(12.dp,4.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = task.name,
+            fontSize = 18.sp,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.weight(1f)
+        )
+        Checkbox(
+            checked = task.status,
+            onCheckedChange = onCheckedChange
+        )
+    }
+}
+
